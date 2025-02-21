669707b26e1d0017023f7acc07270e2374c30fcf
Michael Fisher <fishermichaele@gmail.com>
2025-02-20 22:28:36 -0500

    Fix time sync issue

--- import.py
+++ import.py
@@ -2,10 +2,12 @@
 
 import argparse
 import csv
+import json
 import itertools
 import os
+import math
 from base64 import b64encode
-from datetime import datetime
+from datetime import datetime, timedelta
 from typing import Dict, List
 
 import requests
@@ -14,10 +16,9 @@ WORKSPACE_ID: int = 5534737
 
 toggl_api_key: bytes = f"{os.environ['TOGGL_API_KEY']}:api_token".encode()
 
-
 # Function to add hours for each day
 def add_hours_for_day(rows):
-    total_hours: float = 0.00
+    total_hours: int = 0
     total_cost_amount: float = 0.00
     last_row_per_date = {}
 
@@ -26,12 +27,17 @@ def add_hours_for_day(rows):
         hours = float(data["Hours"])
         cost_amount = float(data["Cost Amount"])
 
+        # Convert hours (1.5) to seconds
+        hours = hours * 3600
+
         # Add hours and cost amount to the totals
         total_hours += hours
         total_cost_amount += cost_amount
 
         # Update the dictionary
-        data["Total Hours"] = total_hours * 3600
+        data["Duration"] = total_hours
+        # Use datetime to add the total_hours to start time
+        data['End Date'] = datetime.strptime(format_datetime(data['Date']), "%Y-%m-%dT%H:%M:%SZ") + timedelta(seconds=total_hours)
         data["Total Cost Amount"] = total_cost_amount
 
         # Store this row as the last row for this date
@@ -57,7 +63,6 @@ def format_datetime(date: str) -> str:
     )
     return datetime.strftime(date, "%Y-%m-%dT%H:%M:%SZ")
 
-
 def group_by_date(sorted_rows):
     # Group the sorted rows by date
     grouped_rows = itertools.groupby(sorted_rows, key=lambda row: row["Date"])
@@ -67,14 +72,11 @@ def group_by_date(sorted_rows):
         yield date, list(rows)
 
 
-def read_file(file: str, project_id: int, project_name: str, workspace_id: int
-              = WORKSPACE_ID, dry_run: bool = False):
+def read_file(file_path: str, project_id: int, project_name: str, workspace_id: int = WORKSPACE_ID, dry_run: bool = False):
+    modified_data: List[Dict[str, str]] = []
 
-    modified_data: List[Dict[bytes, bytes]] = []
-
-    with open(file, "r") as file:
+    with open(file_path, "r") as file:
         reader = csv.DictReader(file)
-        next(reader)
 
         # Sort and group the rows by the first column - Date
         sorted_rows = sorted(reader, key=lambda row: row["Date"])
@@ -83,39 +85,32 @@ def read_file(file: str, project_id: int, project_name: str, workspace_id: int
             for updated_data in add_hours_for_day(rows):
                 modified_data.append(updated_data)
 
-    print("Modified data: ", modified_data)
-
+    dry_run_list = []
     for row in modified_data:
+        payload={
+                "billable": True,
+                "created_with": "python-toggl",
+                "description": "Software development",
+                "project_id": project_id,
+                "start": format_datetime(row["Date"]),
+                # "start_date": format_datetime(row["Date"]),
+                "stop": datetime.strftime(row["End Date"], "%Y-%m-%dT%H:%M:%SZ"),
+                "wid": workspace_id,
+                "workspace_id": workspace_id,
+        }
         if not dry_run:
-            data = requests.post(
+            response = requests.post(
                 f"https://api.track.toggl.com/api/v9/workspaces/{WORKSPACE_ID}/time_entries?meta=true",
-                json={
-                    "billable": True,
-                    "created_with": "python-toggl",
-                    "description": "Software development",
-                    "duration": int(row["Total Hours"]),
-                    "project_id": project_id,
-                    "start": format_datetime(row["Date"]),
-                    "wid": workspace_id,
-                    "workspace_id": workspace_id,
-                },
+                json=payload,
                 headers={
                     "content-type": "application/json",
                     "Authorization": "Basic %s" % b64encode(toggl_api_key).decode("ascii"),
                 },
             )
-            print(data.json())
+            print(response.json())
         else:
-            print(json.pprint({
-                "billable": True,
-                "created_with": "python-toggl",
-                "description": "Software development",
-                "duration": int(row["Total Hours"]),
-                "project_id": project_id,
-                "start": format_datetime(row["Date"]),
-                "wid": workspace_id,
-                "workspace_id": workspace_id,
-            }))
+            dry_run_list.append(payload)
+    print(json.dumps(dry_run_list))
 
 def main():
     parser = argparse.ArgumentParser(description="Parser for Harvest CSV ->\
@@ -133,21 +128,19 @@ def main():
     parser.add_argument('-d', '--dry-run', action='store_true', help='Dry run',
                         required=False)
 
-    # Parsing arguments
     args = parser.parse_args()
-    if args and args.file and args.project and args.name and args.workspace and args.dry_run:
-        filename = args.file
-        project_id = args.project
-        project_name = args.name
-        workspace_id = args.workspace
-        dry_run = args.dry_run
-        read_file(filename, project_id, project_name, workspace_id)
-    elif args and args.file and args.project and args.name and args.dry_run:
-        filename = args.file
-        project_id = args.project
-        project_name = args.name
-        read_file(filename, project_id, project_name)
-        dry_run = args.dry_run
+
+    # Set workspace_id if not provided
+    workspace_id: int = args.workspace if args.workspace else WORKSPACE_ID
+
+    # Parsing arguments
+    if args and args.file and args.project and args.name and workspace_id:
+        filename: str = args.file
+        project_id: int = args.project
+        project_name: str = args.name
+        workspace_id: int = workspace_id
+        dry_run: bool = args.dry_run
+        read_file(filename, project_id, project_name, workspace_id, dry_run if dry_run else False)
     else:
         print("Invalid arguments")
 
